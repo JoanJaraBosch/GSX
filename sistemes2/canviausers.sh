e02d7efc7a2800b4e77711d69a189283f1fbd01c
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2019-03-27 10:54:09 +0100

    script per canviar els usuaris

---
+++ sistemes2/canviausers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Authors: Grup1A
+# Version: 1.0
+# Date: 27/03/2019
+# Description: script que pot rebre qualsevol nombre d'usuaris i els activara o deshabilitara depenent de si existeixen o no. Accepta un sol parametre que es el -h per tal de veure ajuda del script.
+
+
